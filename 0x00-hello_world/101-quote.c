223f25b6731852680d8459b364ffe32a68b4d851
AnasBenba <anasbenba123@gmail.com>
2023-02-16 04:14:13 -0800

    print without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-char msg[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	write(STDERR_FILENO, msg, strlen(msg));
 	return (1);
 }
