399452e56f15278268500be8983ecca2d5aa5551
AnasBenba <anasbenba123@gmail.com>
2023-02-21 08:51:09 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add -> check for the sum of two numbers
+* Return: return the sum of two numbers
+* @n: number to add
+* @s: number to add
+*/
+int add(int n, int s)
+{
+	int sum = n + s;
+
+	return (sum);
+}
