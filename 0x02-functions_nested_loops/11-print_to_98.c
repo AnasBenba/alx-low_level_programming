b1023450589a9ba4e116ee7e8f93178aabd7cd63
AnasBenba <anasbenba123@gmail.com>
2023-02-21 09:12:31 -0800

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_to_98 -> prints number from n to 98
+* @n: number to start
+*/
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+}
