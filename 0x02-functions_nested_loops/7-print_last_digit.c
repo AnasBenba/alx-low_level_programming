830debddf73db9c05746ae0729e44f278febead0
AnasBenba <anasbenba123@gmail.com>
2023-02-21 05:17:35 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include "main"
+
+/**
+* print_last_digit -> check for the last digit
+* Return: return the last digit
+* @n: character to check
+*/
+int print_last_digit(int n)
+{
+	int last = 0;
+	int i = 0;
+
+	while (i < 1)
+	{
+		if (n >= 0)
+		{
+			last = n % 10;
+			_putchar(last + '0');
+			i++;
+		}
+		else
+		{
+			n = n * -1;
+			last = n % 10;
+			_putchar(last + '0');
+			_putchar(last + '0');
+			i++;
+		}
+	}
+}
