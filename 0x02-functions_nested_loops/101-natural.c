51e0ea982e593a6e9f3f3e0f11057e5bc53c7cd1
AnasBenba <anasbenba123@gmail.com>
2023-02-22 07:58:58 -0800

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+* sum_of_multiples -> function that prints the sum of multiples
+* @n: number to use
+* Return: nothing
+*/
+int sum_of_multiples(int n)
+{
+	int sum = 0;
+
+	for (int i = 1; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+}
