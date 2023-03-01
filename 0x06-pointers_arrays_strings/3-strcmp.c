2c7644b5baf845638e9c39a53c3d0163ee90bb64
AnasBenba <anasbenba123@gmail.com>
2023-03-01 09:10:28 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int n = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			n = (s1[i] - s2[i]);
+			break;
 		}
 		i++;
 	}
+	return (n);
 }
