1a260f307b2b4d94115419b41e712356666b1f9a
AnasBenba <anasbenba123@gmail.com>
2023-02-28 22:36:33 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,12 @@
  * _strcmp -> function that compares two strings
  * @s1: pointer to string
  * @s2: pointer to string
+ * Return: nothing
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
