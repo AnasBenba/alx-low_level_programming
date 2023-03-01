af1f0579a718f288ba5f43b202d1a7f15d5c409d
AnasBenba <anasbenba123@gmail.com>
2023-02-28 23:13:58 -0800

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * cap_string -> function that capitalizes all words of a string
+ * @str: pointer to string
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int len = 0;
+	int i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len)
+	{
+		if (str[i] == '\t')
+		{
+			str[i] = ' ';
+		}
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '"')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		else if (str[i] == '!' || str[i] == '?' || str[i] == ')' || str[i] == '{')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		else if (str[i] == '(' || str[i] == '}' || str[i] == '\n' || str[i] == ' ')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
