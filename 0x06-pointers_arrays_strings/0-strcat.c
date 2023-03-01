1c6a42c38fb5209e5ce5c0c52abb697b24abb5bf
AnasBenba <anasbenba123@gmail.com>
2023-02-28 21:53:09 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
