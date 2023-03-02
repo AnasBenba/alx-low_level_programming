46c36f27fec0f03ba4f6bd65779540a785c1e93e
AnasBenba <anasbenba123@gmail.com>
2023-03-02 12:01:42 -0800

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,41 +7,30 @@
  */
 char *cap_string(char *str)
 {
-	int len = 0;
 	int i = 0;
 
-	while (str[len] != '\0')
+	while (str[i])
 	{
-		len++;
-	}
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] = str[0] - 32;
-	while (i < len)
-	{
-		if (str[i] == '\t')
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			str[i] = ' ';
+			i++;
 		}
-		if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '"')
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
-		}
-		else if (str[i] == '!' || str[i] == '?' || str[i] == ')' || str[i] == '{')
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
-		}
-		else if (str[i] == '(' || str[i] == '}' || str[i] == '\n' || str[i] == ' ')
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
+			str[i] -= 32;
 		}
 		i++;
 	}
