b6d7d2b0a50e5e988b1aeaa95eacfd0d31e00bc1
AnasBenba <anasbenba123@gmail.com>
2023-03-06 06:24:15 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strchr -> function that locates a character in a string
+ * @s: pointer to string
+ * @c: character
+ * Return: first occurrence of the character c in the string s, or NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (0);
+}
+
+/**
+ * _strspn -> function that gets the length of a prefix substring
+ * @s: pointer to string
+ * @accept: pointer to string
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		if (_strchr(accept, *p) == NULL)
+		{
+			break;
+		}
+		count++;
+		p++;
+	}
+	return (count);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _strncmp(char *s1, char *s2, int j);
 void print_chessboard(char (*a)[8]);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
