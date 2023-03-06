6b14d440c7f544d8567b7ae71eb266b4fa5f5ffc
AnasBenba <anasbenba123@gmail.com>
2023-03-06 05:33:32 -0800

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _strncmp(char *s1, char *s2, int j);
+void print_chessboard(char (*a)[8]);
 
 #endif
