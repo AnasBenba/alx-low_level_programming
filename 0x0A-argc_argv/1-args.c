b1f208609ba1d568f5c95a05799ef8427c101d07
AnasBenba <anasbenba123@gmail.com>
2023-03-10 02:28:59 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = argc;
+
+	_putchar(count + '0');
+	_putchar('\n');
+	return (0);
+}
