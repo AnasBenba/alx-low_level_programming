5ca7808b1a46c60ab432c115cf769dca4e087bb4
AnasBenba <anasbenba123@gmail.com>
2023-03-13 03:35:45 -0700

    string concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,52 @@
 #include <stdlib.h>
 
 /**
+ * _strcpy - function that copies the string pointed to by src
+ * @src: pointer to string
+ * @dest: pointer to string
+ * Return: dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	while (i < (len + 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * _strcat -> function that concatenates two strings
+ * @dest: pointer to string
+ * @src: pointer to string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
+
+/**
  * str_concat - concatenates two strings.
  * @s1: the first string to concatenate.
  * @s2: the second string to concatenate.
