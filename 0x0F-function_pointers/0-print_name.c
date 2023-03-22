f3acf4e55117bfd46ad6803a52a56cb603b94ceb
AnasBenba <anasbenba123@gmail.com>
2023-03-22 02:21:43 -0700

    function that printf a name using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+* print_name -> prints a name using a function pointer
+*
+* @name: pointer to the name to be printed
+* @f: pointer to the function that will print the name
+*
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+* Pointers to Functions in C
+
+In C, a pointer to a function is a variable that stores the memory address of a function. Pointers to functions are useful when you want to pass a function as an argument to another function, or when you want to store a function as a variable.
