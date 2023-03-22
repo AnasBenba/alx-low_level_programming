fb57ad6122e1a3d43bd0a31d1138b048760a7007
AnasBenba <anasbenba123@gmail.com>
2023-03-22 08:10:27 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+* op_add - function that adds two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: the sum of the two integers
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - function that subtracts two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: the difference of the two integers
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - function that multiplies two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: the product of the two integers
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - function that divides two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: the result of the division of the two integers
+* If @b is 0, the function terminates the program with status code 100
+*/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+* op_mod - function that computes the modulus of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: the modulus of the two integers
+* If @b is 0, the function terminates the program with status code 100
+*/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		exit(100);
+	}
+	return (a % b);
+}
