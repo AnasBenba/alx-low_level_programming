0d6aa918018e1292d57a16fab70e8d1d408992ba
AnasBenba <anasbenba123@gmail.com>
2023-03-23 11:04:19 -0700

    function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,11 +9,17 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+* struct list - struct containing a character and a pointer to a function
+* @c: character
+* @f: pointer to function
+* Description: This struct is used to associate a character with a function
+*              that takes a variable argument list and prints out a value
+*/
 typedef struct list
 {
 	char c;
 	void (*f)(va_list);
-}op;
+} op;
 
 #endif
