91cf3c258a9b1b0c485c911e66be308fc313980b
AnasBenba <anasbenba123@gmail.com>
2023-03-30 05:07:40 -0700

    function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t linked list
+ * @head: pointer to the head node of the list
+ *
+ * Description: This function frees all the nodes in a linked list,
+ * starting from the head node and moving to the end of the list.
+ * It does this by using a temporary pointer to hold the current node,
+ * freeing the current node, and moving to the next node until the end of
+ * the list is reached.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
