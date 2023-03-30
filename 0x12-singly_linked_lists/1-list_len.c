8e35e89a4e6e15c1887586bbac5f36df1676d515
AnasBenba <anasbenba123@gmail.com>
2023-03-30 03:18:25 -0700

    add comment to 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - returns the number of nodes in a linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
 size_t list_len(const list_t *h)
 {
 	int n = 0;
