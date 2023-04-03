1f5bc04a0c47e383b30eadd57edb7eb0a806413d
AnasBenba <anasbenba123@gmail.com>
2023-04-03 09:00:03 -0700

    fix 5-free-listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,17 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	while (*head != NULL)
+	if (*head != NULL)
 	{
-		ptr = *head;
-		*head = (*head)->next;
-		free(ptr);
+		while (*head != NULL)
+		{
+			ptr = *head;
+			*head = (*head)->next;
+			free(ptr);
+		}
+	}
+	else
+	{
+		*head = NULL;
 	}
 }
