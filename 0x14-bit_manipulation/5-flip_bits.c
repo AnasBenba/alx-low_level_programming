bf5807f81f8b9351d41353024e8b66d5dbfd7b22
AnasBenba <anasbenba123@gmail.com>
2023-04-07 04:14:51 -0700

    fix betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the number of bits needed to be flipped to get from one number to another
+ * flip_bits - Counts the number of bits needed to be flipped
+ * to get from one number to another
  * @n: First number
  * @m: Second number
  *
