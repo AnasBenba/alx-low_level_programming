3c36fc1ab2ee837aff39dff76f25be07958a76ad
AnasBenba <anasbenba123@gmail.com>
2023-04-10 03:47:37 -0700

    fix code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, len, numWrite;
+	int file, len = 0, numWrite;
 
 	if (filename == NULL)
 	{
@@ -31,7 +31,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		if (text_content[len])
+		while (text_content[len])
 		{
 			len++;
 		}
