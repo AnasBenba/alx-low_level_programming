e2d49093d77fb10369bc6872b1db3304e87c6791
AnasBenba <anasbenba123@gmail.com>
2023-04-10 03:31:03 -0700

    fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,7 +42,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	len = _strlen(text_contant);
+	len = _strlen(text_content);
 	numWrite = write(file, text_content, len);
 
 	if (numWrite == -1 || numWrite != len)
