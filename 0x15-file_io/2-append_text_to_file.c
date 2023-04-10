3038ef3ebb26c0b7c9f0b28d509610babd489e49
AnasBenba <anasbenba123@gmail.com>
2023-04-10 05:26:11 -0700

    fix code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -52,6 +52,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		numWrite = write(file, text_content, len);
 		if (numWrite == -1 || numWrite != len)
 		{
+			close(file);
 			return (-1);
 		}
 	}
