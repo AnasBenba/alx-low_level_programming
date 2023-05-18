0002349bbf6116b72d0c2a1f9756394a51d968da
AnasBenba <anasbenba123@gmail.com>
2023-05-18 11:11:30 -0700

    fix code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the number of elements in a doubly linked list
- * @h: Pointer to the head of the list
- *
- * Return: Number of elements in the list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t len = 0;
-
-	while (h != NULL)
-	{
-		len++;
-		h = h->next;
-	}
-	return (len);
-}
-/**
  * insert_dnodeint_at_index - Inserts a new node at a
  *                            given position in a doubly linked list.
  * @h: A pointer to the head of the doubly linked list.
@@ -28,44 +11,37 @@ size_t dlistint_len(const dlistint_t *h)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	size_t len = dlistint_len(*h);
-	dlistint_t *ptr, *new;
-	unsigned int i = 0;
+	dlistint_t *ptr = *h, *new;
+	unsigned int i = 1;
 
-	if (h == NULL || *h == NULL)
-		return (NULL);
-	if (idx > len)
-	{
-		return (NULL);
-	}
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-	else if (idx == len)
+	while (i != idx)
 	{
-		return (add_dnodeint_end(h, n));
-	}
-	else
-	{
-		ptr = *h;
-		while (i < idx - 1)
-		{
-			ptr = ptr->next;
-			i++;
-			if (ptr == NULL)
-				return (NULL);
-		}
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
+		ptr = ptr->next;
+		if (ptr == NULL)
 		{
 			return (NULL);
 		}
-		new->n = n;
-		new->prev = ptr;
-		new->next = ptr->next;
-		ptr->next->prev = new;
-		ptr->next = new;
-		return (new);
+		i++;
 	}
+	if (ptr->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = ptr;
+	new->next = ptr->next;
+	ptr->next->prev = new;
+	ptr->next = new;
+
+	return (new);
 }
