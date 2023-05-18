0c0a9605b6b8b8a28f14d7af50c7461a4d2b215f
AnasBenba <anasbenba123@gmail.com>
2023-05-18 09:23:22 -0700

    fix code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,11 +32,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *ptr, *ptr1, *new;
 	unsigned int i = 0;
 
-	if (idx > len)
+	if (h == NULL || *h == NULL)
 	{
 		return (NULL);
 	}
-	if (*h == NULL)
+	if (idx > len)
 	{
 		return (NULL);
 	}
