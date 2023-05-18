4492fbd21a22a3bb6218ec98b667070ce2c0cca7
AnasBenba <anasbenba123@gmail.com>
2023-05-18 05:09:14 -0700

    function that inserts a new node at a given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the number of elements in a doubly linked list
+ * @h: Pointer to the head of the list
+ *
+ * Return: Number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position in a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ * @idx: The index where the new node should be inserted.
+ * @n: The value to be stored in the new node.
+ *
+ * Return: A pointer to the newly inserted node, or NULL if it fails.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	size_t len = dlistint_len(*h);
+	dlistint_t *ptr, *ptr1, *new;
+	unsigned int i = 0;
+
+	if (idx > len)
+	{
+		return (NULL);
+	}
+
+	ptr = *h;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	while (i < idx - 1)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	ptr1 = ptr->next;
+	new->n = n;
+	ptr->next = new;
+	new->next = ptr1;
+	return (new);
+}
