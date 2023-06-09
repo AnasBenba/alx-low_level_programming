bdefb27b2ff548a26bc284a1237e99a0a3fc9d23
AnasBenba <anasbenba123@gmail.com>
2023-06-09 05:56:37 -0700

    fix code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -66,7 +66,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (!strcmp(temp->key, key))
 		{
-			free(value_dup);
 			free(temp->value);
 			temp->value = value_dup;
 			return (1);
