f4050f076270466dcf5918b6d1e12e7c6fd84421
AnasBenba <anasbenba123@gmail.com>
2023-06-09 02:18:06 -0700

    fix code

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	int index;
 	hash_node_t *ptr;
 
-	if (key == NULL || ht == NULL || *key == NULL)
+	if (key == NULL || ht == NULL || *key == '\0')
 	{
 		return (NULL);
 	}
