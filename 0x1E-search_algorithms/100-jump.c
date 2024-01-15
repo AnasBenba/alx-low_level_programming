fed4a8944dc400033bf1a75379ad60c0d70715d0
AnasBenba <anasbenba123@gmail.com>
2024-01-15 19:21:49 +0100

    fix the name of file from O-6 to 6-O and add task 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Searches for a value in a sorted array using jump search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where value is located, or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t start = 0;
+	size_t end = 0;
+	size_t i;
+	int flag = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (array[end] < value && start < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", end, array[end]);
+		start = end;
+		end = end + sqrt(size);
+		if (end > size - 1)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+			end = size;
+			flag = 1;
+		}
+	}
+	if (flag == 0)
+	{
+		printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+		for (i = start; i <= end; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
+	}
+	else
+	{
+		flag = 0;
+		for (i = start; i < end; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,11 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int print_array(int *array, size_t i, size_t r);
+int jump_search(int *array, size_t size, int value);
 
 #endif
